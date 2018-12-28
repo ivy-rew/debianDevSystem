f9ffe27da3779fb36b6218e91ebae432f7424ff3
Reguel <reguel.wermelinger@ivyteam.ch>
2018-12-28 14:49:03 +0100

    script to install remote control & chat tools

---
+++ installers/debCommunicator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# remote-control this workstation
+sudo apt install -y openssh-server
+$DIR/vinoServerVnc.sh
+
+# rdp remote control
+sudo apt install -y remmina
+sudo apt install -y remmina-plugin-rdp
+
+# cooperate 
+$DIR/pidginAndSkype.sh
+$DIR/outlook365/installOutlook.sh
+
+
