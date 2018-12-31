153fdc8c22eaaf51642d599fff173672a8265919
Reguel <reguel.wermelinger@ivyteam.ch>
2018-12-31 10:13:19 +0100

    share 'anonymous' vnc session approach
    opens a blank new session - nice for presentations

--- installers/admin/debRemote.sh
+++ installers/admin/debRemote.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # remote-control this workstation
 sudo apt install -y openssh-server
 $DIR/vinoServerVnc.sh
+$DIR/serveAnonymVncSession.sh
 
 # rdp remote control
 sudo apt install -y remmina
---
+++ installers/admin/serveAnonymVncSession.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# VNC for my running session
+# > tigervnc performs well also on highly animated cinnamon desktops!
+sudo apt install -y tigervnc-standalone-server
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp -v $DIR/serveAnonym_xstartup ~/.vnc/anonymous-vnc_xstartup
+echo 'alias vncHome="vncserver -localhost no -name home -geometry 1366x768 -xstartup $HOME/.vnc/anonymous-vnc_xstartup  :1"' >> ~/.bash_aliases
+echo 'alias vncHomeKill="vncserver -kill :1"' >> ~/.bash_aliases
+source ~/.bash_aliases
+echo 'installed alias: vncHome/vncHomeKill'
