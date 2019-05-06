68c329bd122993445cd26848531c7313cb7b5855
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-05-06 14:53:19 +0200

    add EnhancedClassDecompiler to std plugins

--- installers/dev/eclipse/eclipsePlugins.sh
+++ installers/dev/eclipse/eclipsePlugins.sh
@@ -21,7 +21,13 @@ https://ecd-plugin.github.io/update/ \
 org.eclipse.wb.rcp.feature.feature.group,\
 PathToolsFeature.feature.group,\
 AnyEditTools.feature.group,\
-org.tigris.subversion.subclipse.feature.group \
+org.tigris.subversion.subclipse.feature.group,\
+\
+org.sf.feeling.decompiler,\
+org.sf.feeling.decompiler.cfr,\
+org.sf.feeling.decompiler.jad,\
+org.sf.feeling.decompiler.jd,\
+org.sf.feeling.decompiler.procyon \
 -vmargs -Declipse.p2.mirrors=true -Djava.net.preferIPv4Stack=true
 
 #fix permission issue: eclipse not startable es installed plugins are owned by root
