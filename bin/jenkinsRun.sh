07a16af34c3a8e147c1cd235021b76ba1d6217ab
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-07-19 16:22:19 +0200

    allow selection of target jobs to run on branch

--- bin/jenkinsRun.sh
+++ bin/jenkinsRun.sh
@@ -13,6 +13,8 @@ if [ ! -z "$3" ]
 fi
 
 JENKINS=zugprojenkins
+URL="http://zugprojenkins/job/$JOB/"
+
 
 function triggerBuilds() {
     BRANCH=$1
@@ -22,22 +24,44 @@ function triggerBuilds() {
     echo -n "Enter JENKINS password for $user:" 
     echo -n ""
     read -s password
+    echo ""
 
     CRUMB=`wget -q --auth-no-challenge --user $user --password $password --output-document - 'http://zugprojenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
     echo "GOT CRUMB: " $CRUMB
 
-    BUILD_URL="http://$JENKINS/job/$JOB/job/$BRANCH/build?delay=0sec"
-    curl -I -X POST -u "$user:$password" "$BUILD_URL" -H "$CRUMB"
+    JSON=`curl -s "http://$JENKINS/api/json?tree=jobs[name]"`
+    JOBS=`echo $JSON | jq '.jobs[].name' | grep 'ivy-core_test'`
+    select RUN in none '"ivy-core_ci"' $JOBS
+    do
+        if [ "$RUN" == "none" ]
+        then
+            break
+        fi
+        RUN_JOB=${RUN:1:-1}
+        BUILD_URL="http://$JENKINS/job/$RUN_JOB/job/$BRANCH/build?delay=0sec"
+        curl -I -X POST -u "$user:$password" "$BUILD_URL" -H "$CRUMB"
+    done
 }
 
 #get available jobs: 
-URL="http://zugprojenkins/job/$JOB/"
+function getAvailableBranches()
+{
+  JSON=`curl -s "$URL/api/json?tree=jobs[name]"`
+  BRANCHES=`echo $JSON | jq '.jobs[].name'`
+  echo $BRANCHES
+}
+BRANCHES=$( getAvailableBranches )
+
 echo "SELECT branch of $URL"
-JSON=`curl -s "$URL/api/json?tree=jobs[name]"`
-JOBS=`echo $JSON | jq '.jobs[].name'`
-select BRANCH_SELECTED in $JOBS
+select BRANCH_SELECTED in 're-scan' $BRANCHES
 do
-    triggerBuilds ${BRANCH_SELECTED:1:-1} #kill hyphens
-    break
+    if [ "$BRANCH_SELECTED" == "re-scan" ]
+    then
+        echo 're-scanning [beta]'
+        BRANCHES=$( getAvailableBranches )
+    else
+        triggerBuilds ${BRANCH_SELECTED:1:-1} #kill hyphens
+        break
+    fi
 done
 
