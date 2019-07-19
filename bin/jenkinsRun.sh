eb23e13c312f36b68d04b9530e40345c84443fb7
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-07-19 14:12:06 +0200

    let user select its favourite branch before build

--- bin/jenkinsRun.sh
+++ bin/jenkinsRun.sh
@@ -4,26 +4,40 @@
 # attempt to automate lazy triggered builds that belong to my just PUSHED contents...
 # 
 
-BRANCH=feature%252FXIVY-3272-streamline-defaults
-if [ ! -z "$1" ]
-  then
-    BRANCH=$1
-fi
+SELECT=$1
 
 JOB=ivy-core_ci
-if [ ! -z "$2" ]
+if [ ! -z "$3" ]
   then
-    JOB=$2
+    JOB=$3
 fi
 
 JENKINS=zugprojenkins
 
-echo -n "Enter JENKINS password:\n" 
-read -s password
+function triggerBuilds() {
+    BRANCH=$1
+    echo "triggering builds for $BRANCH"
 
-CRUMB=`wget -q --auth-no-challenge --user rew --password $password --output-document - 'http://zugprojenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
-echo "GOT CRUMB: " $CRUMB
+    user=`whoami`
+    echo -n "Enter JENKINS password for $user:" 
+    echo -n ""
+    read -s password
 
-BUILD_URL="http://$JENKINS/job/$JOB/job/$BRANCH/build?delay=0sec"
-curl -I -X POST -u rew "$BUILD_URL" -H "$CRUMB"
+    CRUMB=`wget -q --auth-no-challenge --user $user --password $password --output-document - 'http://zugprojenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
+    echo "GOT CRUMB: " $CRUMB
+
+    BUILD_URL="http://$JENKINS/job/$JOB/job/$BRANCH/build?delay=0sec"
+    curl -I -X POST -u "$user:$password" "$BUILD_URL" -H "$CRUMB"
+}
+
+#get available jobs: 
+URL="http://zugprojenkins/job/$JOB/"
+echo "SELECT branch of $URL"
+JSON=`curl -s "$URL/api/json?tree=jobs[name]"`
+JOBS=`echo $JSON | jq '.jobs[].name'`
+select BRANCH_SELECTED in $JOBS
+do
+    triggerBuilds ${BRANCH_SELECTED:1:-1} #kill hyphens
+    break
+done
 
