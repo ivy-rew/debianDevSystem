0d7873d7528d094733669ad8c18bdb32a185bb1c
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-07-29 14:55:56 +0200

    allow simpliy RCPTT nightly install via URI parameter (1)

--- installers/dev/eclipse/eclipseRCPTT.sh
+++ installers/dev/eclipse/eclipseRCPTT.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 version=2.4.2
+
+dlUrl=https://mirrors.dotsrc.org/eclipse//rcptt/release/$version/ide/rcptt.ide-$version-linux.gtk.x86_64.zip
+if [ ! -z "$1" ]
+  then
+    # simplify nightly installation via unique URI from https://www.eclipse.org/rcptt/download/
+    dlUrl=$1
+    # version from URI:
+    artifact=`basename $dlUrl`
+    noarch=${artifact%%\-linux\.gtk\.x86_64\.zip}
+    version=${noarch##rcptt\.ide\-}
+fi
+
+echo "Downloading RCPTT IDE $version"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-wget https://mirrors.dotsrc.org/eclipse//rcptt/release/$version/ide/rcptt.ide-$version-linux.gtk.x86_64.zip
+wget $dlUrl
 sudo unzip rcptt.ide*.zip -d /opt/rcptt$version.ide
 rm rcptt.ide*.zip
 
