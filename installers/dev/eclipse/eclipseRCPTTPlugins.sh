db30759b884ffe888538df474083550345dd4522
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-09-03 13:04:30 +0200

    do not install subclipse any longer (all on GIT today)

--- installers/dev/eclipse/eclipseRCPTTPlugins.sh
+++ installers/dev/eclipse/eclipseRCPTTPlugins.sh
@@ -17,14 +17,12 @@ sudo ./rcptt \
 -repository \
 http://download.eclipse.org/releases/photon,\
 http://cdn.rawgit.com/sandipchitale/pathtools/1.0.64/PathToolsUpdateSite/site.xml,\
-http://andrei.gmxhome.de/eclipse/,\
-https://dl.bintray.com/subclipse/releases/subclipse/4.2.x/ \
+http://andrei.gmxhome.de/eclipse/
 -installIUs \
 org.eclipse.epp.mpc.feature.group,\
 org.eclipse.m2e.feature.feature.group,\
 PathToolsFeature.feature.group,\
-AnyEditTools.feature.group,\
-org.tigris.subversion.subclipse.feature.group \
+AnyEditTools.feature.group \
 -vmargs -Declipse.p2.mirrors=true -Djava.net.preferIPv4Stack=true
 
 #fix permission issue: eclipse not startable es installed plugins are owned by root
