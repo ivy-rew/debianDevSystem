714bc40638d0046002b98a751d0e7921b02ad9f3
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-09-05 09:39:45 +0200

    simplify for re-usage under other operating systems
    - remove dep to JQ, but use GREP which also available with cygwin
    - switch to short binary commands: do not use $PATH var as it messes up binary paths following later on
    - de-duplicate code

--- bin/jenkinsGet.sh
+++ bin/jenkinsGet.sh
@@ -11,32 +11,23 @@ ARTIFACT=$4
 ARTIFACT_PATTERN=$5
 
 function jenkinsGet (){
-    # ensure dependent binaries exist
-    if ! [ -x "$(command -v curl)" ]; then
-      sudo apt install -y curl
-    fi
-    if ! [ -x "$(command -v jq)" ]; then
-      sudo apt install -y jq
-    fi
-
-    JSON=`curl -s "http://$JENKINS/job/$JOB/job/$BRANCH/lastSuccessfulBuild/api/json?pretty=true"`
-    ZIP=`echo $JSON | jq -r '.artifacts[].fileName' | grep $ARTIFACT_PATTERN`
-    REVISION=`echo $ZIP | grep -oP '[0-9]{10}'`
+    SUCCESS_URL="http://$JENKINS/job/$JOB/job/$BRANCH/lastSuccessfulBuild"
+    JSON=$(curl -s "$SUCCESS_URL/api/json?pretty=true")
+    REL_PATH=$(echo $JSON | \
+        grep -o '"relativePath" : "[^"]*"' | \
+        grep -o "[^\"]*${ARTIFACT_PATTERN}")
+    ZIP=$(basename "$REL_PATH")
+    REVISION=$(echo $ZIP | grep -oP '[0-9]{10}')
     echo "found revision $REVISION"
 
-    PATH=`echo $JSON | jq -r '.artifacts[].relativePath' | grep $ARTIFACT_PATTERN`
-    URL=http://$JENKINS/job/$JOB/job/$BRANCH/lastSuccessfulBuild/artifact/$PATH
-    NEWZIP=`/usr/bin/wget $URL -P /tmp | grep 'saving to:.*'`
-    echo $NEWZIP
-
+    DL_URL=$SUCCESS_URL/artifact/$REL_PATH
+    wget "$DL_URL" -P /tmp
     echo "Downloaded $ZIP. Enter a description for this $ARTIFACT"
     read DESCRIPTION
 
-
     UNPACKED="/mnt/data/axonIvyProducts/${ARTIFACT}_$REVISION-$DESCRIPTION"
     echo "Extracting to $UNPACKED"
-    /usr/bin/unzip -q "/tmp/$ZIP" -d $UNPACKED
-    cd $UNPACKED
-    `/usr/bin/nemo .`
+    unzip -q "/tmp/$ZIP" -d "$UNPACKED"
+    $(nemo "$UNPACKED" & )
 }
 
