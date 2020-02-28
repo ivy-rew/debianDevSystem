4947ba46e85ac30bc30620d7248d5dbd28eee678
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-02-28 11:13:29 +0100

    colorize compatible with old bash console
    - pre bash 4 impl without readarray so that poor mac users can use this script

--- bin/jenkinsRun.sh
+++ bin/jenkinsRun.sh
@@ -67,12 +67,11 @@ function chooseBranch()
 {
   BRANCHES_RAW=$( getAvailableBranches )
   GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  BRANCHES_COLORED=$(grep -C 100 --color=always -E "${GIT_BRANCH}" <<< "${BRANCHES_RAW}")
+  BRANCHES_COLORED=$(grep -C 100 --color=always -E "${GIT_BRANCH}" <<< "${BRANCHES_RAW[@]}")
   if [[ -z "$BRANCHES_COLORED" ]]; then
-    BRANCHES_COLORED="${BRANCHES_RAW}" #all without highlight: local 'only' branch.
+    BRANCHES_COLORED="${BRANCHES_RAW[@]}" #all without highlight: local 'only' branch.
   fi
-  readarray -t BRANCHES <<< "$BRANCHES_COLORED"
-  OPTIONS=( '!re-scan' '!exit' ${BRANCHES[@]} )
+  OPTIONS=( '!re-scan' '!exit' ${BRANCHES_COLORED[@]} )
 
   echo "SELECT branch of $URL"
   select OPTION in ${OPTIONS[@]}; do
