9e9bc179e00344e4c6a0afa8ccecfbdcf2f018ab
Jacek <lok7ok@gmail.com>
2020-03-12 15:20:24 +0100

    enbale ssh and screen sharing, add commands from ivy-infra

--- installers/mac/macos-dev.sh
+++ installers/mac/macos-dev.sh
@@ -5,6 +5,16 @@
 # turn of akward mouse acceleartion: otherwise host is hard to handle using VNC mouse
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
 
+# enable ssh
+sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+
+# enable vnc/screen sharing
+sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+
+# prefer bash over akward 'zsh': mainly for .bash_profile and .bashrc support
+chsh -s /bin/bash
+
 #homewbrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -15,6 +25,9 @@ brew install htop
 ## homebrew:casks
 brew cask install firefox
 
+#auto-boot configurator
+brew cask install clover-configurator
+
 # jdk 11
 brew cask install homebrew/cask-versions/java11
 
@@ -23,3 +36,5 @@ brew install mvnvm
 
 # git
 brew install git
+
+git config --global core.editor nano
