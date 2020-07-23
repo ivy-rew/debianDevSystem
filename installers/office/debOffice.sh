86dfbdbbdad2ef9565021ff780cf3e65aefea1c6
Reguel <reguel.wermelinger@axonivy.com>
2020-07-23 10:10:55 +0200

    chromium installer for focal + teams by default (#22)

--- installers/office/debOffice.sh
+++ installers/office/debOffice.sh
@@ -5,10 +5,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # cooperate 
 $DIR/pidginAndSkype.sh
+$IDR/installTeams.sh
 $DIR/outlook365/installOutlook.sh
 
 # browse
 $DIR/firefox/installFirefox.sh
+$DIR/installChromium.sh
 
 # screenshot
 $DIR/installShutter.sh
