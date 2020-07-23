c90e8e49215b217e2e79c23159ec56939e4357b4
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-07-23 14:26:11 +0200

    simplified shutter install for focal

--- installers/office/installShutter.sh
+++ installers/office/installShutter.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
+sudo add-apt-repository -y ppa:linuxuprising/shutter
+sudo apt-get update
 sudo apt install -y shutter
-
-# edit support: https://itsfoss.com/shutter-edit-button-disabled/
-sudo apt install -y libextutils-depends-perl
-
-wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
-sudo dpkg -i libgoocanvas3*.deb
-
-wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
-sudo dpkg -i libgoocanvas-common*.deb
-
-wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
-sudo dpkg -i libgoo-canvas-perl*.deb
-
-rm libgoo*.deb
-
-# fix broken deps (mint 19.2):
-sudo apt install -y libextutils-pkgconfig-perl
-
