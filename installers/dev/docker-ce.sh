e0494650385aa8cc0b2ef18c7ff5be6201924991
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-08-04 11:26:43 +0200

    docker with dynamic 'focal' switch for mint

--- installers/dev/docker-ce.sh
+++ installers/dev/docker-ce.sh
@@ -10,10 +10,14 @@ sudo apt install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-#other version than bionic???
-sudo `deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable >> /etc/apt/sources-list`
+CODEBASE=$(lsb_release -cs)
+if [ -f "/etc/upstream-release/lsb-release" ]; then #it's a Linux Mint...
+  CODEBASE=$(grep DISTRIB_CODENAME= /etc/upstream-release/lsb-release | cut -d'=' -f2)
+fi
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   ${CODEBASE} \
+   stable"
 
 sudo apt update
 sudo apt install -y docker-ce
@@ -21,3 +25,6 @@ sudo apt install -y docker-compose
 
 # copy my customizations (e.g. other images dir)
 sudo cp -v docker-daemon.json.template /etc/docker/daemon.json
+
+# Add current user to docker group
+sudo usermod -aG docker "$USER"
