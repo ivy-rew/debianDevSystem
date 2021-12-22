1d7f50e868699a6e180a795d337148d461b1448d
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2021-12-22 21:24:19 +0100

    fixGitCHeckout

--- installers/office/camera/installCamera.sh
+++ installers/office/camera/installCamera.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # overcome broken distrib package -> https://github.com/umlaeute/v4l2loopback
-git checkout https://github.com/umlaeute/v4l2loopback
+git clone https://github.com/umlaeute/v4l2loopback.git
 cd v4l2loopback
 make && sudo make install
 sudo depmod -a
