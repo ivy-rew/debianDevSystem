d10faaeed8ed734f7035f07324bfbd06830a1980
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-01-18 18:48:57 +0100

    re-compile umlaut if module missing

--- installers/office/camera/runBgCam-python.sh
+++ installers/office/camera/runBgCam-python.sh
@@ -4,7 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 loopBack=$(v4l2-ctl --list-devices | grep -A 2 v4l2loopback | grep -o -E /dev/video[0-9]+)
 if [ -z "$loopBack" ]; then
   echo "no loopback device: installing"
-  sudo modprobe v4l2loopback devices=1
+  install=$(sudo modprobe v4l2loopback devices=1)
+  if [ -z "$install" ]; then
+    # re-install for updated kernel
+    "$DIR/installCamera.sh"
+  fi
 fi
 
 cd "${DIR}/Linux-Fake-Background-Webcam"
