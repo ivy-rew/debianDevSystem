2563354900c80d03cfb2bc2614c5e1908622b7b4
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-08-10 13:13:58 +0200

    remove SVN

--- installers/dev/devEnv.sh
+++ installers/dev/devEnv.sh
@@ -17,14 +17,9 @@ sudo apt install -y libgnome-keyring-dev
 cd /usr/share/doc/git/contrib/credential/gnome-keyring && sudo make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
 
-# scm-svn
-sudo apt install -y subversion
-sudo apt install -y nemo-rabbitvcs
-
 # general eclipse deps
 sudo apt install -y libwebkit2gtk-4.0-37
 sudo apt install -y libswt-gtk-4-java
-sudo apt install -y libsvn-java
 
 # composite installers
 $DIR/docker-ce.sh
