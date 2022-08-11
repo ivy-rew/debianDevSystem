f81606ce36276824ad2169da8e3e75cb390b53db
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-08-11 08:18:38 +0200

    distinct VNC alias setup

--- installers/admin/serveAnonymVncSession.sh
+++ installers/admin/serveAnonymVncSession.sh
@@ -6,8 +6,7 @@ sudo apt install -y tigervnc-standalone-server
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -v $DIR/serveAnonym_xstartup ~/.vnc/anonymous-vnc_xstartup
-echo 'alias vncHome="vncserver -localhost no -name home -geometry 1366x768 -xstartup $HOME/.vnc/anonymous-vnc_xstartup  :3"' >> ~/.bash_aliases
-echo 'alias vncHomeKill="vncserver -kill :3"' >> ~/.bash_aliases
-source ~/.bash_aliases
-echo 'installed alias: vncHome/vncHomeKill'
+cp -v $DIR/serveAnonym_xstartup $HOME/.vnc/anonymous-vnc_xstartup
+cat vncAliases | tee -a $HOME/.bash_aliases
+source $HOME/.bash_aliases
+echo "installed aliases: $(cat vncAliases)"
