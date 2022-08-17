2f099b591e5f3d2f0adcdeacb489fc0985a8c6c6
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-08-17 10:21:20 +0200

    batch mode install for firefox

--- installers/office/firefox/installFirefoxPpa.sh
+++ installers/office/firefox/installFirefoxPpa.sh
@@ -15,4 +15,4 @@ Pin-Priority: 1001
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";'\
  | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
-sudo apt install firefox
+sudo apt install y firefox
