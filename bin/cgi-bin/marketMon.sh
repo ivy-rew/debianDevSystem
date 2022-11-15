9c27d599fba2b52e20b191573eb35734d47c1bd8
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-11-15 11:37:21 +0100

    serve the monitor over http

---
+++ bin/cgi-bin/marketMon.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+org="axonivy-market"
+
+collectRepos() {
+  curl "https://api.github.com/orgs/${org}/repos?per_page=100" | 
+  jq -r '.[] | 
+    select(.archived == false) | 
+    select(.is_template == false) | 
+    select(.default_branch == "master") | 
+    select(.language != null) | 
+    .name'
+}
+
+print() {
+  collectRepos |
+  while read repo_name; do
+    status $repo_name
+  done
+}
+
+status() {
+  repo=$1
+  build="https://github.com/${org}/${repo}/actions/workflows/ci.yml"
+  badge="${build}/badge.svg"
+  echo "<li><a href='${build}'><img src='${badge}'/> ${repo}</a></li>"
+}
+
+page() {
+  echo "<html><ul>"
+  print
+  echo "</ul></html>"
+}
+
+localFile() {
+  html="/tmp/marketmon.html"
+  page > $html
+  firefox $html
+}
+
+localPage() {
+  echo "Content-type: text/html"
+  echo ""
+  page
+}
+
+localPage
--- bin/marketMon.sh
+++ bin/marketMon.sh
@@ -1,41 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
-org="axonivy-market"
+DIR="$( cd "$( dirname "${BASH_SOURCE}" )" && pwd )"
+cd $DIR
 
-collectRepos() {
-  curl "https://api.github.com/orgs/${org}/repos?per_page=100" | 
-  jq -r '.[] | 
-    select(.archived == false) | 
-    select(.is_template == false) | 
-    select(.default_branch == "master") | 
-    select(.language != null) | 
-    .name'
-}
-
-print() {
-  collectRepos |
-  while read repo_name; do
-    status $repo_name
-  done
-}
-
-status() {
-  repo=$1
-  build="https://github.com/${org}/${repo}/actions/workflows/ci.yml"
-  badge="${build}/badge.svg"
-  echo "<li><a href='${build}'><img src='${badge}'/> ${repo}</a></li>"
-}
-
-page() {
-  echo "<html><ul>"
-  print
-  echo "</ul></html>"
-}
-
-localFile() {
-  html="/tmp/marketmon.html"
-  page > $html
-  firefox $html
-}
-
-localFile
+python3 -m http.server --cgi
+firefox localhost:8000/cgi-bin/marketMon.sh
