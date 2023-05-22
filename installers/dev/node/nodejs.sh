fcc3d89695418189bd571ce591bdf602d9f7a837
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-05-22 09:26:35 +0200

    with default version set

--- installers/dev/node/nodejs.sh
+++ installers/dev/node/nodejs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#node-version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+version=16.20
+nvm use "${version}"
+nvm alias default "v${version}"
 
-nvm use 16.20
 npm install -g yarn
