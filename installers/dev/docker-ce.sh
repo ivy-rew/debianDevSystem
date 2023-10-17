eb16e9d638efd8c0aa33bdb78e6decf3e9d1c2b6
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-10-17 08:46:45 +0200

    functions for isolated runs

--- installers/dev/docker-ce.sh
+++ installers/dev/docker-ce.sh
@@ -2,30 +2,37 @@
 
 #https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1
 
-sudo apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+dockerPkg(){
+  sudo apt install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
 
-dockerGpg=/etc/apt/trusted.gpg.d/docker.gpg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o $dockerGpg
+  dockerGpg=/etc/apt/trusted.gpg.d/docker.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o $dockerGpg
 
-CODEBASE=$(lsb_release -cs)
-if [ -f "/etc/upstream-release/lsb-release" ]; then #it's a Linux Mint...
-  CODEBASE=$(grep DISTRIB_CODENAME= /etc/upstream-release/lsb-release | cut -d'=' -f2)
-fi
-sudo add-apt-repository \
-   "deb [arch=amd64 signedBy=${dockerGpg}] https://download.docker.com/linux/ubuntu \
-   ${CODEBASE} \
-   stable"
+  CODEBASE=$(lsb_release -cs)
+  if [ -f "/etc/upstream-release/lsb-release" ]; then #it's a Linux Mint...
+    CODEBASE=$(grep DISTRIB_CODENAME= /etc/upstream-release/lsb-release | cut -d'=' -f2)
+  fi
+  sudo add-apt-repository \
+    "deb [arch=amd64 signedBy=${dockerGpg}] https://download.docker.com/linux/ubuntu \
+    ${CODEBASE} \
+    stable"
+}
 
-sudo apt update
-sudo apt install -y docker-ce
-sudo apt install -y docker-compose
+dockerInst(){
+  sudo apt update
+  sudo apt install -y docker-ce
+  sudo apt install -y docker-compose
 
-# copy my customizations (e.g. other images dir)
-sudo cp -v docker-daemon.json.template /etc/docker/daemon.json
+  # copy my customizations (e.g. other images dir)
+  sudo cp -v docker-daemon.json.template /etc/docker/daemon.json
 
-# Add current user to docker group
-sudo usermod -aG docker "$USER"
+  # Add current user to docker group
+  sudo usermod -aG docker "$USER"
+}
+
+dockerPkg
+dockerInst
