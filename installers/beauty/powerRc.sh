c975c29c4fbdadf8004c61d228ebfca0c6eb9bed
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-01-18 10:50:51 +0100

    fix: run powerline deamon only once
    - not on each and every bash open

--- installers/beauty/powerRc.sh
+++ installers/beauty/powerRc.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ -f `which powerline-daemon` ]; then
-    powerline-daemon -q
-    POWERLINE_BASH_CONTINUATION=1
-    POWERLINE_BASH_SELECT=1
-fi
-
 updateTitle(){
   prefix="\u@\h: "
   if [[ -z "$SSH_CLIENT" ]]; then
--- installers/beauty/powerline.sh
+++ installers/beauty/powerline.sh
@@ -9,6 +9,14 @@ ohMyBash(){
 
 standalone(){
   sudo apt install -y powerline powerline-gitstatus fonts-powerline
+  tee -a $HOME/.profile << EOF
+# powerline
+if [ -f `which powerline-daemon` ]; then
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
+fi
+EOF
   echo "source ${DIR}/powerRc.sh" | tee -a $HOME/.bashrc
 }
 
