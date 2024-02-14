45ad99ce2c6733eb570b6d4830e4748625a86fd3
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-02-14 18:33:34 +0100

    fix: anonym-vnc installer from other dir

--- installers/admin/serveAnonymVncSession.sh
+++ installers/admin/serveAnonymVncSession.sh
@@ -6,7 +6,8 @@ sudo apt install -y tigervnc-standalone-server
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -v $HOME/.vnc
 cp -v $DIR/serveAnonym_xstartup $HOME/.vnc/anonymous-vnc_xstartup
-cat vncAliases | tee -a $HOME/.bash_aliases
+cat ${DIR}/vncAliases | tee -a $HOME/.bash_aliases
 source $HOME/.bash_aliases
 echo "installed aliases: $(cat vncAliases)"
