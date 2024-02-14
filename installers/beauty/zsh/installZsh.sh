15ef5b632cae366c74f7240cf3bb87ec0da14c5e
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-02-14 12:18:51 +0100

    install powerlevel10k by default

--- installers/beauty/zsh/installZsh.sh
+++ installers/beauty/zsh/installZsh.sh
@@ -3,6 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt install -y zsh
+# as default shell:
+chsh -s $(which zsh)
 
 source "${DIR}/powerline10k.sh"
 ohMyZsh
--- installers/beauty/zsh/powerline10k.sh
+++ installers/beauty/zsh/powerline10k.sh
@@ -7,8 +7,17 @@ ohMyZsh() {
 }
 
 pwrLine10k() {
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  cp ${DIR}/p10k.sh $HOME/.p10k.sh
+  P10Kdir=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  if [ ! -d ${P10Kdir} ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${P10Kdir}
+  fi
+  cp -v ${DIR}/p10k.zsh $HOME/.p10k.zsh
+  tee -a $HOME/.zshrc << EOF
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+EOF
+  sed -i 's|ZSH_THEME=".*|ZSH_THEME="powerlevel10k/powerlevel10k"|' $HOME/.zshrc
 }
 
 nvmPlugin() {
