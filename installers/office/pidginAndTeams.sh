363b32a6639596e3cba69e311f2d8933805a18b4
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-02-14 18:36:43 +0100

    install pidgin teams from other dir

--- installers/office/pidginAndTeams.sh
+++ installers/office/pidginAndTeams.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo apt install -y pidgin
 
 ### add skype ###
 sudo apt install -y libjson-glib-dev libpurple-dev
-git clone https://github.com/EionRobb/purple-teams.git
-cd purple-teams
+git clone https://github.com/EionRobb/purple-teams.git ${DIR}/purple-teams
+cd ${DIR}/purple-teams
 make
 sudo make install
 
