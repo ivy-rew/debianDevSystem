8c3c7b0b6cbb98f227ee3278f281c3c9652c053e
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-02-15 09:37:53 +0100

    fix: outlook chrome-app installer
    - with icon
    - injecting path to debian dev
    - keep outlook.sh close to its contrib

--- installers/office/outlook365/installOutlook.sh
+++ installers/office/outlook365/installOutlook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo apt install -y chromium-browser
 
-cp -v outlook365.desktop $HOME/.local/share/applications/outlook365.desktop
+cat "${DIR}/outlook365.desktop"\
+ | sed "s|outlook.sh|${DIR}/outlook.sh|"\
+ | sed "s|outlook365.png|${DIR}/outlook365.png|"\
+  > $HOME/.local/share/applications/outlook365.desktop
