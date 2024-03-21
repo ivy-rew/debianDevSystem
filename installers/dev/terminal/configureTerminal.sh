bead9f84e96027d21887ed93144609b0544eefa8
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-03-21 13:34:48 +0100

    fix: streamline zsh-bash setup

--- installers/dev/terminal/configureTerminal.sh
+++ installers/dev/terminal/configureTerminal.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cat $DIR/dev-bash_aliases | tee -a $HOME/.bash_aliases
-
 termDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cat <<EOF | tee -a $HOME/.bashrc
+cat $termDir/dev-bash_aliases | tee -a $HOME/.bash_aliases
+# route ZSH to bash-aliases
+echo "source \$HOME/.bash_aliases" | tee -a $HOME/.zshrc 
+
+
+cat <<EOF | tee -a $HOME/.bashrc | tee -a $HOME/.zshrc
 
 # git-functs
 source ${termDir}/gitOp.sh
