7772dc07e1ffe2dc00cb69e985c4f36f77880b9c
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-03-21 13:43:17 +0100

    add my 'bin' dir to path

--- installers/dev/terminal/configureTerminal.sh
+++ installers/dev/terminal/configureTerminal.sh
@@ -2,14 +2,32 @@
 
 termDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cat $termDir/dev-bash_aliases | tee -a $HOME/.bash_aliases
-# route ZSH to bash-aliases
-echo "source \$HOME/.bash_aliases" | tee -a $HOME/.zshrc 
+devAliases(){
+  cat $termDir/dev-bash_aliases | tee -a $HOME/.bash_aliases
+  # route ZSH to bash-aliases
+  echo "source \$HOME/.bash_aliases" | tee -a $HOME/.zshrc 
+}
 
-
-cat <<EOF | tee -a $HOME/.bashrc | tee -a $HOME/.zshrc
+sourceGit(){
+  cat <<EOF | tee -a $HOME/.bashrc | tee -a $HOME/.zshrc
 
 # git-functs
 source ${termDir}/gitOp.sh
 
 EOF
+}
+
+devBinaries(){
+  devBin=$(realpath ${termDir}/../../../bin)
+  cat <<EOF | tee -a $HOME/.profile
+
+# include debianDevSystem
+if [ -d "${devBin}" ]; then
+      PATH="${devBin}:\$PATH"
+fi
+EOF
+}
+
+devAliases
+sourceGit
+devBinaries
