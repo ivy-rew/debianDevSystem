8270fea4f4923a95288010253decad37f2bae65e
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-03-21 13:29:29 +0100

    source gitOp.sh regardless of the cloned location

--- installers/dev/terminal/configureTerminal.sh
+++ installers/dev/terminal/configureTerminal.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cat $DIR/dev-bashrc | tee -a $HOME/.bashrc
 cat $DIR/dev-bash_aliases | tee -a $HOME/.bash_aliases
+
+termDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cat <<EOF | tee -a $HOME/.bashrc
+
+# git-functs
+source ${termDir}/gitOp.sh
+
+EOF
