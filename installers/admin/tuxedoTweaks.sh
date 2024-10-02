32ebf12348ef9fd268a85ffdaefb4481b29503d2
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-10-02 09:01:06 +0200

    tuxedo pulse

---
+++ installers/admin/tuxedoTweaks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tuxedoRepo(){
+  CODEBASE=$(lsb_release -cs)
+  if [ -f "/etc/upstream-release/lsb-release" ]; then #it's a Linux Mint...
+    CODEBASE=$(grep DISTRIB_CODENAME= /etc/upstream-release/lsb-release | cut -d'=' -f2)
+  fi
+
+  echo "deb https://deb.tuxedocomputers.com/ubuntu ${CODEBASE} main" | sudo tee /etc/apt/sources.list.d/tuxedo-computers.list
+  wget https://deb.tuxedocomputers.com/ubuntu/pool/main/t/tuxedo-archive-keyring/tuxedo-archive-keyring_2022.04.01~tux_all.deb
+  sudo dpkg -i tuxedo-archive-keyring_2022.04.01~tux_all.deb
+  sudo apt update && apt search tuxedo 
+}
+
+tuxedoRepo
+
+sudo apt install -y tuxedo-control-center tuxedo-drivers
