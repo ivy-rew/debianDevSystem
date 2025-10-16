ee4d1eb9aa9df629739f912f7656ffbb02b4f3d7
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-10-16 09:42:31 +0200

    Update vscode repository installation

--- installers/dev/vs-code/installer.sh
+++ installers/dev/vs-code/installer.sh
@@ -3,10 +3,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 instPkg() {
+  # https://code.visualstudio.com/docs/setup/linux
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ${DIR}/packages.microsoft.gpg
-  sudo install -o root -g root -m 644 ${DIR}/packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-  sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo install -o root -g root -m 644 ${DIR}/packages.microsoft.gpg /usr/share/keyrings/microsoft.gpg
   rm ${DIR}/packages.microsoft.gpg
+  
+  sudo tee /etc/apt/trusted.d/vscode.source > /dev/null <<EOF
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+EOF
 
   sudo apt update
   sudo apt install -y code
